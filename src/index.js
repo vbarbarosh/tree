bad8f70a30928d9ab615129e889d2a6bccf84938
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-03-16 01:34:10 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import tree_flatten from './tree_flatten';
 import tree_from_array from './tree_from_array';
 import tree_from_string from './tree_from_string';
+import tree_from_string2 from './tree_from_string2';
 import tree_print from './tree_print';
+import tree_print2 from './tree_print2';
 import tree_random from './tree_random';
 import tree_sort_preorder from './tree_sort_preorder';
 import tree_stringify from './tree_stringify';
 import tree_walk2 from './tree_walk2';
 import tree_walk_preorder from './tree_walk_preorder';
+import tree_walk_preorder2 from './tree_walk_preorder2';
 
 export {
     tree_flatten,
     tree_from_array,
     tree_from_string,
+    tree_from_string2,
     tree_print,
+    tree_print2,
     tree_random,
     tree_sort_preorder,
     tree_stringify,
     tree_walk2,
     tree_walk_preorder,
+    tree_walk_preorder2,
 };

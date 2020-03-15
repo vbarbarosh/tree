270acfe268555e5564a9e554b98cfe46b7a457ba
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-03-15 19:50:27 +0200

    rename fcmp_tree_str to fcmp_tree_ids

--- src/tree_sort.js
+++ src/tree_sort.js
@@ -1,10 +1,10 @@
-function tree_sort(children, fcmp = fcmp_tree_str)
+function tree_sort(children, fcmp = fcmp_tree_ids)
 {
     children.sort(fcmp).forEach(v => tree_sort(v.children, fcmp));
     return children;
 }
 
-function fcmp_tree_str(a, b)
+function fcmp_tree_ids(a, b)
 {
     return a.id.localeCompare(b.id);
 }

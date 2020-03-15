ca4a42c9257df0c6d8f4565955cc4d343ca3f094
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-03-15 23:21:14 +0200

    rename fcmp_tree_ids to fcmp_nodes_id

--- src/tree_sort_preorder.js
+++ src/tree_sort_preorder.js
@@ -1,10 +1,10 @@
-function tree_sort_preorder(children, fcmp = fcmp_tree_ids)
+function tree_sort_preorder(children, fcmp = fcmp_nodes_id)
 {
     children.sort(fcmp).forEach(v => tree_sort_preorder(v.children, fcmp));
     return children;
 }
 
-function fcmp_tree_ids(a, b)
+function fcmp_nodes_id(a, b)
 {
     return a.id.localeCompare(b.id);
 }

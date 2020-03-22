d4b0b8e9f536cdc2469142b96a812714a588f321
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-03-23 00:44:48 +0200

    add tree_diff

---
+++ src/tree_diff.js
@@ -0,0 +1,32 @@
+import array_index from './array_index';
+import tree_from_array from './tree_from_array';
+import tree_walk_preorder2 from './tree_walk_preorder2';
+
+/**
+ * Return nodes not presented in selection (descendants are counted).
+ *
+ * @param nodes
+ * @param selection
+ * @return {array} An array of removed nodes.
+ */
+function tree_diff(nodes, selection)
+{
+    const keep = [];
+    const selection_map = array_index(selection, v => v.id);
+    tree_walk_preorder2({
+        nodes: tree_from_array(nodes.map(function (node, i) {
+            return {id: node.id, parent_id: node.parent_id, value: node, i};
+        })),
+        visit: function ({node}) {
+            if (selection_map[node.parent_id]) {
+                selection_map[node.id] = true;
+            }
+            if (!selection_map[node.id]) {
+                keep.push(node.value);
+            }
+        },
+    });
+    return keep;
+}
+
+export default tree_diff;
---
+++ src/tree_diff.m.js
@@ -0,0 +1,41 @@
+import tree_flatten from './tree_flatten';
+import tree_from_string2 from './tree_from_string2';
+import tree_diff from './tree_diff';
+
+describe('tree_diff', function () {
+    it('should handle empty arrays', function () {
+        assert.deepEqual(tree_diff([], []), []);
+    });
+    it('should handle basic input', function () {
+        const a = tree(`
+            a
+            b
+            c
+        `);
+        assert.deepEqual(tree_diff(a, [{id:'a'}]), [{id:'b',parent_id:null},{id:'c',parent_id:null}]);
+        assert.deepEqual(tree_diff(a, [{id:'a'},{id:'b'}]), [{id:'c',parent_id:null}]);
+        assert.deepEqual(tree_diff(a, [{id:'b'},{id:'a'}]), [{id:'c',parent_id:null}]);
+    });
+    it('should count descendants', function () {
+        const a = tree(`
+            a
+              b
+                c
+            d
+            e
+        `);
+        assert.deepEqual(tree_diff(a, [{id:'a'}]), [{id:'d',parent_id:null},{id:'e',parent_id:null}]);
+    });
+});
+
+function tree(text)
+{
+    const out = tree_flatten(tree_from_string2(text));
+    const ids = {};
+    out.forEach(v => ids[v.id] = v.text);
+    out.forEach(v => v.id = ids[v.id]);
+    out.forEach(v => v.parent_id = ids[v.parent_id]);
+    out.forEach(v => delete v.level);
+    out.forEach(v => delete v.text);
+    return out;
+}

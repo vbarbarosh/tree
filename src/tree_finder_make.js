64f0ab88057d5948419f323db8ab79762ca86ff2
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-04-02 15:53:09 +0300

    update tree_finder_make

--- src/tree_finder_make.js
+++ src/tree_finder_make.js
@@ -31,11 +31,11 @@ function update(ctx, x, y, container_rect = ctx.container_rect)
     x += sx;
     y += sy;
 
-    const closest_i = numbers_closest_i_first(ctx.node_rects.map(v => v.y).concat(ctx.container_rect.y+ctx.container_rect.h), y);
+    const closest_i = numbers_closest_i_first(ctx.node_rects.map(v => v.y).concat(ctx.container_rect0.y+ctx.container_rect.h), y);
     const closest_rect = ctx.node_rects[closest_i] || {
-        x: (ctx.node_rects[numbers_min_i_first(ctx.node_rects.map(v => v.y))]||ctx.container_rect).x,
-        y: ctx.container_rect.y + ctx.container_rect.h,
-        w: ctx.container_rect.w,
+        x: (ctx.node_rects[numbers_min_i_first(ctx.node_rects.map(v => v.y))]||ctx.container_rect0).x,
+        y: ctx.container_rect0.y + ctx.container_rect.h,
+        w: ctx.container_rect0.w,
         h: 0,
         node: null,
     };

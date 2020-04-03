cd3971215c49809743704f33b4c99ce37536825b
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-04-03 18:26:34 +0300

    deprecated tree_stringify

--- src/tree_stringify.js
+++ src/tree_stringify.js
@@ -1,5 +1,13 @@
 import tree_walk_preorder from './tree_walk_preorder';
 
+/**
+ *
+ * @param children
+ * @param cb
+ * @returns {string}
+ *
+ * @deprecated This is not the same as JSON.stringify; deprecated in favor of tree_print
+ */
 function tree_stringify(children, cb = child => child.id)
 {
     const rows = [];

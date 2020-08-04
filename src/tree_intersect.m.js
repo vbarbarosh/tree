834f501ab76e4681a0c3948e22f470e4b3c1e845
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-08-04 12:51:15 +0300

    fix tree helper

--- src/tree_intersect.m.js
+++ src/tree_intersect.m.js
@@ -31,11 +31,6 @@ describe('tree_intersect', function () {
 function tree(text)
 {
     const out = tree_flatten(tree_from_string(text));
-    const ids = {};
-    out.forEach(v => ids[v.id] = v.text);
-    out.forEach(v => v.id = ids[v.id]);
-    out.forEach(v => v.parent_id = ids[v.parent_id]||null);
-    out.forEach(v => delete v.level);
-    out.forEach(v => delete v.text);
+    out.forEach(v => delete v.name);
     return out;
 }
--- src/tree_move.m.js
+++ src/tree_move.m.js
@@ -64,11 +64,6 @@ function dup(value)
 function tree(text)
 {
     const out = tree_flatten(tree_from_string(text));
-    const ids = {};
-    out.forEach(v => ids[v.id] = v.text);
-    out.forEach(v => v.id = ids[v.id]);
-    out.forEach(v => v.parent_id = ids[v.parent_id]||null);
-    out.forEach(v => delete v.level);
-    out.forEach(v => delete v.text);
+    out.forEach(v => delete v.name);
     return out;
 }

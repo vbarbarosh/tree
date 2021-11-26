0bbd7996a8330f31b38726ce77164eb527aa30e1
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2021-11-26 22:03:01 +0200

    get rid of faker dependency

--- src/tree_random.js
+++ src/tree_random.js
@@ -1,18 +1,16 @@
-import faker from 'faker';
-
-function tree_random(depth = 3)
+function tree_random(depth = 3, random_word = () => faker.lorem.word)
 {
     const used = {};
-    return nodes(1, depth, used);
+    return nodes(1, depth, used, random_word);
 }
 
-function nodes(depth, end, used)
+function nodes(depth, end, used, random_word)
 {
     const lines = [];
     for (let i = 0, ii = Math.floor(Math.random()*5) + 1; i < ii; ++i) {
         let w;
         for (let j = 0; j < 50; ++j) {
-            if (!used[w = faker.lorem.word()]) {
+            if (!used[w = random_word()]) {
                 break;
             }
         }
@@ -22,7 +20,7 @@ function nodes(depth, end, used)
         used[w] = true;
         lines.push(' '.repeat(depth*4) + w);
         if (depth < end) {
-            lines.push(nodes(depth + 1, end, used));
+            lines.push(nodes(depth + 1, end, used, random_word));
         }
     }
     return lines.join('\n');
